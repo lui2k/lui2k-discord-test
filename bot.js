fe47b2e9796e475669cee60295bbe3a3457a985e
lui2k <37487891+lui2k@users.noreply.github.com>
2018-04-15 09:12:35 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,15 +3,23 @@ const client = new Discord.Client();
 
 var items = ['cache','cobble','inferno','mirage','nuke','overpass','train'];
 var PFitems = ['cache','cobble','inferno','mirage','nuke','overpass','train','subzero','dust2','canals'];
+var user = "";
 
 
 client.on('ready', () => {
     console.log('I am ready!');
+    client.user.setActivity({game: {name: "CS:GO", type:0}});
+    client.user.setStatus("online");
 });
 
 client.on('message', message => {
     if (message.content.toLowerCase() === '!mapveto' || message.content.toLowerCase() === '!map veto') {
         message.reply('Enter !ActiveDutyVeto  OR  !PopflashVeto');
+        user = message.mentions.members.first();
+    }
+    
+    if(message.content.toLowerCase() === '!mapUser') {
+        message.reply(user + " is the active user");
     }
 });
 
