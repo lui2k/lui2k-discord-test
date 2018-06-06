44b6bbb755ab5fb888aedd3a189ffd607ff1c0a2
lui2k <37487891+lui2k@users.noreply.github.com>
2018-06-06 01:21:35 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const fs = require('fs');
+const dataFile = require("./data.json");
+
 const token = 'NDM0OTg1NjMwMTA0MDI3MTM3.DbSX1w.P343t6FMU81sJczI-m_67XoEFoc';
 
 var items = ['cache','cobble','inferno','mirage','nuke','overpass','train'];
@@ -7,6 +10,9 @@ var items = ['cache','cobble','inferno','mirage','nuke','overpass','train'];
 client.on('message', message => {
     if (message.content.toLowerCase() === '!lui2k') {
         message.reply('Running test bot');
+        dataFile.num = (config.num + 1);
+        
+        fs.writeFile("./data.json",JSON.stringify(dataFile), (err) => console.error);
     }
 });
 
