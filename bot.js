bdaebeb4afd4cee009f49cae488143be4a998d43
lui2k <37487891+lui2k@users.noreply.github.com>
2018-06-06 02:14:50 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,13 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-var fs = require("fs");
-
 const token = 'NDM0OTg1NjMwMTA0MDI3MTM3.DbSX1w.P343t6FMU81sJczI-m_67XoEFoc';
 
 client.on('message', message => {
-    if (message.content.toLowerCase() === '!lui2k') {        
-        fs.writeFile("/Storage/userInfo.txt", 'new text');
-        message.reply('Running test bot');
+    if (message.content.toLowerCase() === '!mirage') {        
+        message.reply("some text", {
+            file: "https://vignette.wikia.nocookie.net/cswikia/images/a/a7/CSGO_de_Mirage.jpg"
+        });
+
     }
 });
 
