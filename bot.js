36eaaa91df8a0008d5b6b287d82a0051f259a633
lui2k <37487891+lui2k@users.noreply.github.com>
2018-06-06 02:00:00 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,21 +5,14 @@ var userData = JSON.parse(fs.readFileSync('Storage/userInfo.json', 'utf8'));
 
 const token = 'NDM0OTg1NjMwMTA0MDI3MTM3.DbSX1w.P343t6FMU81sJczI-m_67XoEFoc';
 
-var items = ['cache','cobble','inferno','mirage','nuke','overpass','train'];
-
 client.on('message', message => {
-    if (message.content.toLowerCase() === '!lui2k') {
-        message.reply('Running test bot');
-        
+    if (message.content.toLowerCase() === '!lui2k') {        
         if(!userData[message.author.id]) userData[message.author.id] = {
                 messagesSent: 0
             }
-        
         userData[message.author.id].messagesSent ++;
-        
-        fs.writeFile("Storage/userInfo.json", JSON.stringify(userData), (err) => {
-            if(err) console.error(err);
-        });
+        fs.writeFile("Storage/userInfo.json", JSON.stringify(userData));
+        message.reply('Running test bot');
     }
 });
 
