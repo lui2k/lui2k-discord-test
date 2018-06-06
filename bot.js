d024be91d4f3764f87e9f5507b9e6f4a3be67fc4
lui2k <37487891+lui2k@users.noreply.github.com>
2018-06-06 02:06:23 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-var fs = require('fs');
+var fs = require("fs");
 var userData = JSON.parse(fs.readFileSync('Storage/userInfo.json', 'utf8'));
 
 const token = 'NDM0OTg1NjMwMTA0MDI3MTM3.DbSX1w.P343t6FMU81sJczI-m_67XoEFoc';
@@ -10,7 +10,7 @@ client.on('message', message => {
         if(!userData[message.author.id]) userData[message.author.id] = {
                 messagesSent: 0
             }
-        userData[message.author.id].messagesSent ++;
+        userData[message.author.id].messagesSent += 1;
         fs.writeFile("Storage/userInfo.json", JSON.stringify(userData));
         message.reply('Running test bot');
     }
