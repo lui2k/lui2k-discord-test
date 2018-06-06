1435b73c50c3d400cc66427a1385972d87ff370d
lui2k <37487891+lui2k@users.noreply.github.com>
2018-06-06 02:08:26 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,17 +1,13 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 var fs = require("fs");
-var userData = JSON.parse(fs.readFileSync('Storage/userInfo.json', 'utf8'));
+var userData = JSON.parse(fs.readFileSync('Storage/userInfo.txt', 'utf8'));
 
 const token = 'NDM0OTg1NjMwMTA0MDI3MTM3.DbSX1w.P343t6FMU81sJczI-m_67XoEFoc';
 
 client.on('message', message => {
     if (message.content.toLowerCase() === '!lui2k') {        
-        if(!userData[message.author.id]) userData[message.author.id] = {
-                messagesSent: 0
-            }
-        userData[message.author.id].messagesSent += 1;
-        fs.writeFile("Storage/userInfo.json", JSON.stringify(userData));
+        fs.writeFile("Storage/userInfo.txt", 'new text');
         message.reply('Running test bot');
     }
 });
