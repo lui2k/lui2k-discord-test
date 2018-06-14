29a8e1e23ff2f652eb584fe8b37f9642da8c84a1
lui2k <37487891+lui2k@users.noreply.github.com>
2018-06-14 17:23:31 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 const token = 'NDM0OTg1NjMwMTA0MDI3MTM3.DbSX1w.P343t6FMU81sJczI-m_67XoEFoc';
 
 client.on('message', message => {
-    if (message.content.toLowerCase().startsWith("!scrimAdd"))
+    if (message.content.toLowerCase().startsWith("!scrimadd"))
     {        
         message.reply("allowed to add.");
     }
