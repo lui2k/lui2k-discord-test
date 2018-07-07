e43f333f1c6af8ddfc6c4dd766a037bd2b387d29
lui2k <37487891+lui2k@users.noreply.github.com>
2018-07-07 10:09:13 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,75 +1,286 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const token = 'NDM0OTg1NjMwMTA0MDI3MTM3.DbSX1w.P343t6FMU81sJczI-m_67XoEFoc';
-var correctLength = false, map = false, region=false;
+
+var items = ['cache','dust2','inferno','mirage','nuke','overpass','train'];
+var PFitems = ['cache','cobble','inferno','mirage','nuke','overpass','train','subzero','dust2','canals'];
+var pickSide = ['heads','tails'];
+var user;
+var maps;
+var mapsLeft;
+var allowBan = false;
+var bestOfSelected = false;
+var bestOf;
+
 
 client.on('message', message => {
-    if (message.content.toLowerCase().startsWith("!scrimadd"))
-    {
-        correctLength=false;
-        map=false;
-        region=false;
-        var info = message.content.toLowerCase();
-        info = info.split(" ");
-        
-        if(info.length==4)
-        { correctLength = true; }
-        
-        if(info[3].toString().match("(any|cache|cobblestone|dust2|mirage|inferno|overpass|nuke|train)"))
-        { map=true; }
-        
-        if(info[2].toString().match("(eu|na|sa|afr|oce|asia)"))
-        { region=true; }
-        
-        if(correctLength && map && region)
+	if (message.content.toLowerCase() === '!mapveto' || message.content.toLowerCase() === '!map veto'  || message.content.toLowerCase() === '!veto') {
+		message.reply('Enter !veto bestOfOne  OR  !veto bestOfThree');
+		mapsLeft = 50;
+		bestOfSelected=false;
+		allowBan =false;
+	}
+});
+
+client.on('message', message=> {
+	if(message.content.toLowerCase() === '!veto flipcoin' || message.content.toLowerCase() === '!veto flipacoin')
+	{
+		message.reply('the coin has landed on '+ pickSide[Math.floor(Math.random()*pickSide.length)] + '.');
+	}
+});
+
+
+client.on('message', message => {
+    if (message.content.toLowerCase() === '!veto bestofone' && !allowBan && !bestOfSelected) {
+	    bestOf = 1;
+	    bestOfSelected=true;
+	    allowBan =false;
+	    message.reply('Enter !Veto ActiveDuty  OR  !Veto Popflash for a best of ' + bestOf + "match.");
+    }
+});
+
+client.on('message', message => {
+    if (message.content.toLowerCase() === '!veto bestofthree' && !allowBan && !bestOfSelected) {
+	    bestOf = 3;
+	    bestOfSelected=true;
+	    allowBan =false;
+	    message.reply('Enter !Veto ActiveDuty  OR  !Veto Popflash for a best of ' + bestOf + "match.");
+    }
+});
+
+
+client.on('message', message => {
+    if (message.content.toLowerCase() === '!veto activeduty' && !allowBan && bestOfSelected) {
+        maps = 'cache, dust2, inferno, mirage, nuke, overpass, train';
+        message.reply('Active Duty Map Veto starting: Type !veto MapName to ban any of the following maps: ' + maps);
+        mapsLeft = 7; 
+        allowBan=true;
+    }
+});
+
+client.on('message', message => {
+    if (message.content.toLowerCase() === '!veto popflash'  && !allowBan && bestOfSelected) {
+	    maps = 'subzero, dust2, canals, cobble, cache, inferno, mirage, nuke, overpass, train';
+	    message.reply('Popflash Map Veto starting: Type !veto MapName to ban any of the following maps: ' + maps);
+	    mapsLeft = 10; 
+	    allowBan=true;
+    }
+});
+
+
+//MAPS LEFT CMD
+client.on('message', message => {
+	if (message.content.toLowerCase() === '!mapsleft' && allowBan) {
+		message.reply("Maps left: "+maps + " ("+(mapsLeft - 1)+")");
+	}
+});
+
+//BOT HELP CMD
+client.on('message', message => {
+    if (message.content.toLowerCase() === '!veto help') {
+	    message.reply("Need help with the Veto Bot? Visit http://csgoscrims.co.uk/discordBot.html or contact the developer: Lui2k#3225 ");
+    }
+});
+
+
+//MAPS
+client.on('message', message => {
+    if (message.content.toLowerCase() === '!veto cobble' && maps.indexOf('cobble')!= -1 && allowBan) {
+        maps = maps.replace('cobble, ', '');
+        message.reply('Cobblestone removed. Maps left: ' + maps);
+        mapsLeft -= 1; 
+        if(mapsLeft==bestOf)
         {
-            var str = "http://csgoscrims.co.uk/scrimDiscordAdd.php?teamName=" + info[1] + "&user=" + message.author + "&region=" + info[2] + "&map=" + info[3];
-            var xml = new XMLHttpRequest();
-            xml.open("GET", str, false);
-            xml.send();
-            message.reply("Uploading:" + xml.responseText);
-            return;
-        }
-        else
-        {
-            message.reply("Error. Try again with the following format:  !scrimAdd TeamName Map Region. \\n \\n E.g. !scrimAdd TeamA Dust2 EU");
-            return;
+            message.reply("you will play on " + maps + " (BO" + bestOf + "). Good luck, have fun!" );
+            allowBan=false;
+            bestOfSelected=false;
         }
     }
+});
+
+client.on('message', message => {
+    if (message.content.toLowerCase() === '!veto cache' && maps.indexOf('cache')!= -1 && allowBan) {
+        maps = maps.replace('cache, ', '');
+        mapsLeft -= 1; 
+        message.reply('Cache removed. Maps left: ' + maps);
+        if(mapsLeft==bestOf)
+        {
+            message.reply("you will play on " + maps + " (BO" + bestOf + "). Good luck, have fun!" );
+            allowBan=false;
+            bestOfSelected=false;
+        }
+    }
+});
+
+client.on('message', message => {
+    if (message.content.toLowerCase() === '!veto mirage' && maps.indexOf('mirage')!= -1 && allowBan) {
+        maps = maps.replace('mirage, ', '');
+        mapsLeft -= 1; 
+        message.reply('Mirage removed. Maps left: ' + maps);
+        if(mapsLeft==bestOf)
+        {
+		message.reply("you will play on " + maps + " (BO" + bestOf + "). Good luck, have fun!")
+		allowBan=false;
+		bestOfSelected=false;
+        }
+    }
+});
+
+
+client.on('message', message => {
+    if (message.content.toLowerCase() === '!veto nuke' && maps.indexOf('nuke')!= -1 && allowBan) {
+        maps = maps.replace('nuke, ', '');
+        message.reply('Nuke removed. Maps left: ' + maps);
+        mapsLeft -= 1; 
+        if(mapsLeft==bestOf)
+        {
+            message.reply("you will play on " + maps + " (BO" + bestOf + "). Good luck, have fun!" );
+            allowBan=false;
+            bestOfSelected=false;
+        }
+    }
+});
+
+
+
+client.on('message', message => {
+    if (message.content.toLowerCase() === '!veto overpass' && maps.indexOf('overpass')!= -1 && allowBan) {
+        maps = maps.replace('overpass, ', '');
+        message.reply('Overpass removed. Maps left: ' + maps);
+        mapsLeft -= 1; 
+        if(mapsLeft==bestOf)
+        {
+            message.reply("you will play on " + maps + " (BO" + bestOf + "). Good luck, have fun!" );
+            allowBan=false;
+            bestOfSelected=false;
+        }
+    }
+});
+
+
+client.on('message', message => {
+    if (message.content.toLowerCase() === '!veto train' && maps.indexOf('train')!= -1 && allowBan) {
+        maps = maps.replace('train', '');
+        message.reply('Train removed. Maps left: ' + maps);
+        mapsLeft -= 1; 
+        if(mapsLeft==bestOf)
+        {
+            message.reply("you will play on " + maps + " (BO" + bestOf + "). Good luck, have fun!" );
+            allowBan=false;
+            bestOfSelected=false;
+        }
+    }
+});
+
+
+client.on('message', message => {
+    if (message.content.toLowerCase() === '!veto inferno' && maps.indexOf('inferno')!= -1 && allowBan ) {
+        maps = maps.replace('inferno, ', '');
+        message.reply('Inferno removed. Maps left: ' + maps);
+        mapsLeft -= 1; 
+        if(mapsLeft==bestOf)
+        {
+            message.reply("you will play on " + maps + " (BO" + bestOf + "). Good luck, have fun!" );
+            allowBan=false;
+            bestOfSelected=false;
+        }
+    }
+});
+
+
+client.on('message', message => {
+    if (message.content.toLowerCase() === '!veto canals' && maps.indexOf('canals')!= -1 &&allowBan) {
+        maps = maps.replace('canals, ', '');
+        message.reply('Canals removed. Maps left: ' + maps);
+        mapsLeft -= 1; 
+        if(mapsLeft==bestOf)
+        {
+            message.reply("you will play on " + maps + " (BO" + bestOf + "). Good luck, have fun!" );
+            allowBan=false;
+            bestOfSelected=false;
+        }
+    }
+});
+
+client.on('message', message => {
+    if (message.content.toLowerCase() === '!veto subzero' && maps.indexOf('subzero')!= -1 &&allowBan) {
+        maps = maps.replace('subzero, ', '');
+        message.reply('Subzero removed. Maps left: ' + maps);
+        mapsLeft -= 1; 
+        if(mapsLeft==bestOf)
+        {
+            message.reply("you will play on " + maps + " (BO" + bestOf + "). Good luck, have fun!" );
+            allowBan=false;
+            bestOfSelected=false;
+        }
+        
+    }
+});
+
+client.on('message', message => {
+    if (message.content.toLowerCase() === '!veto dust2' && maps.indexOf('dust2')!= -1 &&allowBan) {
+        maps = maps.replace('dust2, ', '');
+        message.reply('Dust 2 removed. Maps left: ' + maps);
+        mapsLeft -= 1; 
+        if(mapsLeft==bestOf)
+        {
+            message.reply("you will play on " + maps + " (BO" + bestOf + "). Good luck, have fun!" );
+            allowBan=false;
+            bestOfSelected=false;
+        }
+    }
+});
+
+
+client.on('message',message=> {
+    if(message.content.toLowerCase() === '!randommap' || message.content.toLowerCase() === '!random map' ) {
+     message.reply( items[Math.floor(Math.random()*items.length)] );
+    }
+});
+
+
+
+client.on('message',message=> {
+    if(message.content.toLowerCase() === '!randompopflashmap' || message.content.toLowerCase() === '!random popflash map' ) {
+     message.reply( PFitems[Math.floor(Math.random()*PFitems.length)] );
+    }
+});
+
+
+///EAL-SPECIFIC MAP VETO (Contact @Lui2k_ for specific)
+var ealVeto = false, coinFlipped = false;
+var userA, userB;
+
+client.on('message', message => {
+	if (message.content.toLowerCase() === '!ealmapveto' || message.content.toLowerCase() === '!eal map veto'  || message.content.toLowerCase() === '!eal veto') {
+		message.reply('Running the EAL Map Veto. //n //n Each team leader predicts a coin flip: Type !ealveto heads  OR  !ealveto tails.');
+		mapsLeft = 50;
+		bestOfSelected=false;
+		allowBan =false;
+        coinFlipped=false;
+        ealVeto = true;
+	}
     
-    if(message.content.toLowerCase().startsWith("!scrimfind"))
+    if (message.content.toLowerCase() === '!ealveto heads' || message.content.toLowerCase() === '!eal veto heads'  || message.content.toLowerCase() === '!eal heads') {
+        userA = message.author.toString()
+		message.channel.send(userA +" has selected heads.");
+	}
+    else if (message.content.toLowerCase() === '!ealveto tails' || message.content.toLowerCase() === '!eal veto tails'  || message.content.toLowerCase() === '!eal tails') {
+        userB = message.author.toString()
+		message.channel.send(userB +" has selected tails.");
+	}
+    
+    if(userA != "" && userB != "")
     {
-        correctLength=false;
-        map=false;
-        
-        var info = message.content.toLowerCase();
-        info = info.split(" ");
-        
-        if(info.length==2)
-        { correctLength=true;}
-        
-        if(info[1].toString().match("(any|cache|cobblestone|dust2|mirage|inferno|overpass|nuke|train)"))
-        { map=true; }
-               
-        if(correctLength && map)
-        {
-            var str = "http://csgoscrims.co.uk/scrimDiscordReturn.php?map=" + info[1];
-            var xml = new XMLHttpRequest();
-            xml.open("GET", str, false);
-            xml.send();
-            message.reply("Uploading:" + xml.responseText);
-            return;
-        }
-        else
-        {
-            message.reply("Error. Try again with the following format:  !scrimFind Map \\n \\n E.g. !scrimFind Inferno");
-            return;
-        }
+        message.channel.send(FlipCoin);
     }
     
     
 });
 
+function FlipCoin()
+{
+    return pickSide[Math.floor(Math.random()*pickSide.length)];
+}
 
-client.login(token);
-//client.login(process.env.BOT_TOKEN);
+    
+client.login(process.env.BOT_TOKEN);
