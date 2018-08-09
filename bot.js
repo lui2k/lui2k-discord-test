4cb17d0295efc6af97b04733633e4a80bf5752ed
lui2k <37487891+lui2k@users.noreply.github.com>
2018-08-09 16:52:17 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,12 +12,10 @@ var bestOfSelected = false;
 var bestOf;
 
 
-client.on("ready", () => {
-  // This event will run if the bot starts, and logs in, successfully.
-  console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
-  // Example of changing the bot's playing game to something useful. `client.user` is what the
-  // docs refer to as the "ClientUser".
-  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+client.on('message', message => {
+	if (message.content.toLowerCase() === '!veto stats') {
+		message.reply('Serving ${client.guilds.size} servers');
+	}
 });
 
 client.on('message', message => {
