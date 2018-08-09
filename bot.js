43261af266a9be46244cd1013608fbb8edce16c0
lui2k <37487891+lui2k@users.noreply.github.com>
2018-08-09 16:48:19 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,6 +12,14 @@ var bestOfSelected = false;
 var bestOf;
 
 
+client.on("ready", () => {
+  // This event will run if the bot starts, and logs in, successfully.
+  console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
+  // Example of changing the bot's playing game to something useful. `client.user` is what the
+  // docs refer to as the "ClientUser".
+  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+});
+
 client.on('message', message => {
 	if (message.content.toLowerCase() === '!mapveto' || message.content.toLowerCase() === '!map veto'  || message.content.toLowerCase() === '!veto') {
 		message.reply('Enter !veto bestOfOne  OR  !veto bestOfThree');
