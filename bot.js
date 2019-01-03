b844c5327e61faa885bd415cadc234881eb17d52
lui2k <37487891+lui2k@users.noreply.github.com>
2019-01-03 15:17:49 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,10 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('message', message => {
-	if (message.content.toLowerCase() === '!ping') {
-		message.reply("Pong!");
-	});
-    
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+
 client.login(process.env.BOT_TOKEN);
